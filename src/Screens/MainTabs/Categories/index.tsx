a0bbde96cefcd66df42b5c43c35abd435bff9742
Kauã Steliano <stelianok@gmail.com>
2022-09-06 17:24:09 -0300

    Created main screens files

---
+++ src/Screens/MainTabs/Categories/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styles from '../Search/styles';
+
+const { body } = styles;
+export default function Cattegories() {
+  return (
+    <View style={body}>
+      <Text>Cattegories</Text>
+    </View>
+  );
+}
---
+++ src/Screens/MainTabs/Favorites/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styles from '../Search/styles';
+
+const { body } = styles;
+export default function Favorites() {
+  return (
+    <View style={body}>
+      <Text>Favorites</Text>
+    </View>
+  );
+}
