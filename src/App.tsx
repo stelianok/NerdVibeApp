29066fe48433bc89b8e73f78f5b7d61e7c02b6bd
Kauã Steliano <stelianok@gmail.com>
2022-09-06 16:09:26 -0300

    Installed necessary dependencies, moved App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+
+import Home from './Screens/Home';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Home />
+    </NavigationContainer>
+  );
+};
+
+export default App;
