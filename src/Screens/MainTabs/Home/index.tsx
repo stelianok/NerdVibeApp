d4f2c98c38349b9e725af2d964148f1b151b82f3
Kauã Steliano <stelianok@gmail.com>
2022-09-06 20:45:17 -0300

    Created Clean blank style

--- src/Screens/MainTabs/Home/index.tsx
+++ src/Screens/MainTabs/Home/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import GlobalStyles from '../../../GlobalStyles';
 import styles from './styles';
 
-const { Title, TitleWithPoppins } = styles;
+const { body } = styles;
+const { h3 } = GlobalStyles;
 export default function Home() {
   return (
-    <View>
-      <Text style={Title}>Home</Text>
-      <Text style={TitleWithPoppins}>Home</Text>
+    <View style={body}>
+      <Text style={[{ color: '#000', ...h3 }]}>h3</Text>
     </View>
   );
 }
--- src/Screens/MainTabs/Home/styles.ts
+++ src/Screens/MainTabs/Home/styles.ts
@@ -1,11 +1,10 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  TitleWithPoppins: {
-    fontFamily: 'Poppins-Regular',
-  },
-  Title: {
-    fontFamily: 'Roboto',
+  body: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
