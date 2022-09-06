ea9127bf542cf0e71f141af7b63d2330b7a877ee
Kauã Steliano <stelianok@gmail.com>
2022-09-06 20:21:53 -0300

    Configured Global Font Styles

---
+++ src/GlobalStyles.ts
@@ -0,0 +1,51 @@
+import { StyleSheet } from 'react-native';
+
+const GlobalStyles = StyleSheet.create({
+  h3: {
+    fontFamily: 'Poppins-Regular',
+    fontSize: 48,
+  },
+  h4: {
+    fontFamily: 'Poppins-Regular',
+    fontSize: 34,
+  },
+
+  h5: {
+    fontFamily: 'Poppins-Regular',
+    fontSize: 24,
+  },
+  h6: {
+    fontFamily: 'Poppins-Medium',
+    fontSize: 20,
+  },
+  subtitle1: {
+    fontFamily: 'Poppins-Regular',
+    fontSize: 16,
+  },
+  subtitle2: {
+    fontFamily: 'Poppins-Medium',
+    fontSize: 0,
+  },
+  bodyFont1: {
+    fontFamily: 'Poppins-Regular',
+    fontSize: 16,
+  },
+  bodyFont2: {
+    fontFamily: 'Poppins-Regular',
+    fontSize: 14,
+  },
+  buttonFont: {
+    fontFamily: 'Poppins-Medium',
+    fontSize: 14,
+  },
+  Caption: {
+    fontFamily: 'Poppins-Regular',
+    fontSize: 12,
+  },
+  overline: {
+    fontFamily: 'Poppins-Regular',
+    fontSize: 10,
+  },
+});
+
+export default GlobalStyles;
