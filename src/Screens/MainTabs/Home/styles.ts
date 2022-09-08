d59c1d5c5493e59f1eb9469aacf585869f29a83f
Kauã Steliano <stelianok@gmail.com>
2022-09-08 09:53:29 -0300

    Changed Home Background Color

--- src/Screens/MainTabs/Home/styles.ts
+++ src/Screens/MainTabs/Home/styles.ts
@@ -6,7 +6,7 @@ const styles = StyleSheet.create({
   },
   body: {
     flex: 1,
-    backgroundColor: '#ff6b99',
+    backgroundColor: '#fff',
     justifyContent: 'center',
     alignItems: 'center',
   },
