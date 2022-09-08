b09f3cfe1560b4e59c75d6d9d227ce098fd9bca4
Kauã Steliano <stelianok@gmail.com>
2022-09-08 09:52:41 -0300

    Stylized status bar

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,18 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 
 import MainTabs from './Screens/MainTabs';
+import { StatusBar } from 'react-native';
 
 const App = () => {
   return (
     <NavigationContainer>
       <GestureHandlerRootView style={{ flex: 1 }}>
-        {<MainTabs />}
+        {
+          <>
+            <StatusBar backgroundColor={'#fff'} barStyle={'dark-content'} />
+            <MainTabs />
+          </>
+        }
       </GestureHandlerRootView>
     </NavigationContainer>
   );
