e0fb5f4e34e5c219a7a899464e2ef6d5f8ea4e74
Kauã Steliano <stelianok@gmail.com>
2022-09-08 09:24:39 -0300

    Implemente Header on home

--- src/Screens/MainTabs/Home/index.tsx
+++ src/Screens/MainTabs/Home/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import Header from '../../../components/Header';
 import GlobalStyles from '../../../GlobalStyles';
 import styles from './styles';
 
-const { body } = styles;
+const { container, body, titleBody } = styles;
 const { h3 } = GlobalStyles;
+
 export default function Home() {
   return (
-    <View style={body}>
-      <Text style={[{ color: '#000', ...h3 }]}>h3</Text>
+    <View style={container}>
+      <Header />
+      <View style={body}>
+        <Text style={[titleBody, h3]}>h3</Text>
+      </View>
     </View>
   );
 }
--- src/Screens/ShoppingCart/styles.ts
+++ src/Screens/ShoppingCart/styles.ts
@@ -1,17 +1,11 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
   body: {
-    flex: 1,
-    backgroundColor: '#ff6b99',
     justifyContent: 'center',
     alignItems: 'center',
-  },
-  titleBody: {
-    color: '#000',
+    flex: 1,
+    backgroundColor: 'cyan',
   },
 });
 
