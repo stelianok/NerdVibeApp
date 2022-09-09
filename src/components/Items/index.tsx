5af5c7e7ba6e93d47bbd7e7ffaa392ccb84d544c
Kauã Steliano <stelianok@gmail.com>
2022-09-09 14:15:41 -0300

    Created ItemGrid Component boilerplate

---
+++ src/components/Items/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native';
+import styles from './styles';
+
+const { container } = styles;
+export default function ItemsGrid() {
+  return <View style={container} />;
+}
