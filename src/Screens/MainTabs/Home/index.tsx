8015ff3907c439e00cc00b09b5d71c480d9cd44b
Kauã Steliano <stelianok@gmail.com>
2022-09-12 10:42:17 -0300

    Now data is passed correctly

--- src/Screens/MainTabs/Home/index.tsx
+++ src/Screens/MainTabs/Home/index.tsx
@@ -9,15 +9,53 @@ import styles from './styles';
 const { container, body } = styles;
 
 export default function Home() {
+  const data: IProduct[] = [
+    {
+      imageURL:
+        'https://cdn.shopify.com/s/files/1/1375/9903/products/threefaces_finalvendetta_blackt-shirt_pl_1_720x720.jpg?v=1657776237',
+      name: 'Camisa Braba',
+      price: '27.00',
+      isOnSale: false,
+    },
+    {
+      imageURL:
+        'https://cdn.shopify.com/s/files/1/1375/9903/products/jetsetradio_hoodie_1_1024x1024.jpg?v=1657837433',
+      name: 'Moletom',
+      price: '275.00',
+      isOnSale: true,
+    },
+    {
+      imageURL:
+        'https://cdn.shopify.com/s/files/1/1375/9903/products/ns563678stw_1_720x720.jpg?v=1657866244',
+      name: 'Meia daora',
+      price: '3.00',
+      isOnSale: false,
+    },
+    {
+      imageURL:
+        'https://cdn.shopify.com/s/files/1/1375/9903/products/hd162783spn1_720x720.jpg?v=1657844625',
+      name: 'Moletom miranha',
+      price: '250.00',
+      isOnSale: true,
+    },
+    {
+      imageURL:
+        'https://cdn.shopify.com/s/files/1/1375/9903/products/ts223330sky_600x.jpg?v=1657862996',
+      name: 'Monke',
+      price: '375.00',
+      isOnSale: true,
+    },
+  ];
+
   return (
     <View style={container}>
       <Header />
       <View style={body}>
         <ScrollView>
           <ItemsGrid />
-          <Items sectionTitle="Recently Added" />
-          <Items sectionTitle="Popular right now" />
-          <Items sectionTitle="On Sale" />
+          <Items sectionTitle="Recently Added" products={data} />
+          <Items sectionTitle="Popular right now" products={data} />
+          <Items sectionTitle="On Sale" products={data} />
         </ScrollView>
       </View>
     </View>
--- src/components/Items/index.tsx
+++ src/components/Items/index.tsx
@@ -5,55 +5,18 @@ import Item from './Item';
 import styles from './styles';
 const { container, title, itemsContainer, itemsFlatlist } = styles;
 
-const data: IProduct[] = [
-  {
-    imageURL:
-      'https://cdn.shopify.com/s/files/1/1375/9903/products/threefaces_finalvendetta_blackt-shirt_pl_1_720x720.jpg?v=1657776237',
-    name: 'Camisa Braba',
-    price: '27.00',
-    isOnSale: false,
-  },
-  {
-    imageURL:
-      'https://cdn.shopify.com/s/files/1/1375/9903/products/jetsetradio_hoodie_1_1024x1024.jpg?v=1657837433',
-    name: 'Moletom',
-    price: '275.00',
-    isOnSale: true,
-  },
-  {
-    imageURL:
-      'https://cdn.shopify.com/s/files/1/1375/9903/products/ns563678stw_1_720x720.jpg?v=1657866244',
-    name: 'Meia daora',
-    price: '3.00',
-    isOnSale: false,
-  },
-  {
-    imageURL:
-      'https://cdn.shopify.com/s/files/1/1375/9903/products/hd162783spn1_720x720.jpg?v=1657844625',
-    name: 'Moletom miranha',
-    price: '250.00',
-    isOnSale: true,
-  },
-  {
-    imageURL:
-      'https://cdn.shopify.com/s/files/1/1375/9903/products/ts223330sky_600x.jpg?v=1657862996',
-    name: 'Monke',
-    price: '375.00',
-    isOnSale: true,
-  },
-];
-
 interface IProps {
   sectionTitle: string;
+  products: IProduct[];
 }
-export default function Items({ sectionTitle }: IProps) {
+export default function Items({ sectionTitle, products }: IProps) {
   return (
     <View style={container}>
       <Text style={[title, GlobalStyles.h6]}>{sectionTitle}</Text>
       <View style={itemsContainer}>
         <FlatList
           horizontal
-          data={data}
+          data={products}
           renderItem={({ item }) => (
             <Item
               imageURL={item.imageURL}
