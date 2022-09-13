43b5ff9b09a34cff86ce5df18d1c0bb83424e124
Kauã Steliano <stelianok@gmail.com>
2022-09-13 09:49:11 -0300

    Created Product Detailed Screen

---
+++ src/Screens/ProductDetailed/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styles from './styles';
+
+const { body } = styles;
+export default function ProductDetailed() {
+  return (
+    <View style={body}>
+      <Text>Product Detailed</Text>
+    </View>
+  );
+}
