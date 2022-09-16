c72741e8d32fb7ff1518484cc34bea4594d49d9d
Kauã Steliano <stelianok@gmail.com>
2022-09-16 09:58:10 -0300

    Created image carousel for product detailed screen

--- src/Screens/ProductDetailedTopTabs/ProductDetailedOverview/index.tsx
+++ src/Screens/ProductDetailedTopTabs/ProductDetailedOverview/index.tsx
@@ -1,13 +1,117 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, Dimensions, ScrollView } from 'react-native';
+
+import Swiper from 'react-native-swiper';
+
+import Icon from 'react-native-vector-icons/FontAwesome';
+import GlobalStyles from '../../../GlobalStyles';
 import styles from './styles';
 
-const { body } = styles;
+const {
+  container,
+  header,
+  body,
+  carouselContainer,
+  imageStyle,
+  mainInfoContainer,
+  rowInfo,
+  TitleInfoContainer,
+  price,
+  productTitle,
+  reviewContainer,
+  reviewIcon,
+  reviewText,
+} = styles;
+
+interface IData {
+  id: string;
+  imageURL: string;
+}
+const data: IData[] = [
+  {
+    id: '1',
+    imageURL:
+      'https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/91d6AcW-krL._SX385_.jpg',
+  },
+  {
+    id: '2',
+    imageURL:
+      'https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/61B8Lq5NXmL._AC_SY450_.jpg',
+  },
+
+  {
+    id: '3',
+    imageURL:
+      'https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/91d6AcW-krL._SX385_.jpg',
+  },
+  {
+    id: '4',
+    imageURL:
+      'https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/91d6AcW-krL._SX385_.jpg',
+  },
+  {
+    id: '5',
+    imageURL:
+      'https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/61B8Lq5NXmL._AC_SY450_.jpg',
+  },
+
+  {
+    id: '6',
+    imageURL:
+      'https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/91d6AcW-krL._SX385_.jpg',
+  },
+];
 
 export default function ProductDetailedOverview() {
   return (
-    <View style={body}>
-      <Text>Product Detailed Overview</Text>
+    <View style={container}>
+      <ScrollView>
+        <View style={header}>
+          <Swiper containerStyle={carouselContainer}>
+            <Image
+              source={{ uri: data[0].imageURL }}
+              style={imageStyle}
+              resizeMode={'contain'}
+            />
+            <Image
+              source={{ uri: data[1].imageURL }}
+              style={imageStyle}
+              resizeMode={'contain'}
+            />
+            <Image
+              source={{ uri: data[2].imageURL }}
+              style={imageStyle}
+              resizeMode={'contain'}
+            />
+            <Image
+              source={{ uri: data[3].imageURL }}
+              style={imageStyle}
+              resizeMode={'contain'}
+            />
+          </Swiper>
+        </View>
+        <View style={body}>
+          <View style={mainInfoContainer}>
+            <View style={rowInfo}>
+              <View style={TitleInfoContainer}>
+                <Text style={[price, GlobalStyles.h5]}>$ 200</Text>
+                <Text style={[productTitle, GlobalStyles.h4]}>
+                  Produto oficial
+                </Text>
+              </View>
+              <View style={reviewContainer}>
+                <Icon
+                  name={'star'}
+                  color={'yellow'}
+                  size={32}
+                  style={reviewIcon}
+                />
+                <Text style={[reviewText, GlobalStyles.h4]}>4.8</Text>
+              </View>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 }
--- src/Screens/ProductDetailedTopTabs/ProductDetailedOverview/styles.ts
+++ src/Screens/ProductDetailedTopTabs/ProductDetailedOverview/styles.ts
@@ -1,12 +1,55 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
+
+const height = Dimensions.get('window').height;
 
 const styles = StyleSheet.create({
-  body: {
-    justifyContent: 'center',
-    alignItems: 'center',
+  container: {
     flex: 1,
-
-    backgroundColor: 'blue',
+    backgroundColor: '#fff',
+  },
+  header: {
+    backgroundColor: '#dedede',
+    marginBottom: 5,
+  },
+  body: {
+    margin: 15,
+  },
+  carouselContainer: {
+    margin: 5,
+    marginTop: -25,
+    height: height / 1.9,
+  },
+  imageStyle: {
+    width: '100%',
+    height: '100%',
+    alignSelf: 'center',
+  },
+  mainInfoContainer: {},
+  rowInfo: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  TitleInfoContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  price: {
+    color: '#03B252',
+    fontWeight: 'bold',
+  },
+  productTitle: {
+    color: '#000',
+  },
+  reviewContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+  },
+  reviewIcon: {
+    marginBottom: 15,
+  },
+  reviewText: {
+    color: '#000',
   },
 });
 
